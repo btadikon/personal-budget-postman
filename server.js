13aa432544870d5bc667ce6d3fa2bed7efce8219
Bharadwaaja Tadikonda <btadikon@uncc.edu>
2024-03-03 23:35:11 -0500

    Completed MongoDB Instructions

--- server.js
+++ server.js
@@ -1,31 +1,67 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const data = require("./data.json");
+const budget = require("./data.json");
 
 app.use('/', express.static('public'));
+const cors = require('cors');
 
-const budget = [
-    {
-      title: 'Eat out',
-      budget: 30
-    },
-    {
-      title: 'Rent',
-      budget: 300  
-    },
-    {
-      title: 'Groceries',
-      budget: 90  
-    },
-];
-app.get('/hello', (req, res) => {
-    res.send('Hello World!');
-});
+app.use(cors());
+
+const mongoose = require('mongoose')
+let url = 'mongodb://localhost:27017/personal-budget';
+
+const budgetModel = require("./models/schema");
+
+app.use('/' , express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/budget', (req, res) => {
-    res.send(data);
+    mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => {
+            console.log("connected to database");
+            budgetModel.find({})
+                .then((data) => {
+                    // console.log(data);
+                    res.send(data);
+                    mongoose.connection.close();
+                })
+                .catch((connectionError) => {
+                    console.log(connectionError)
+                })
+        })
+        .catch((connectionError) => {
+            console.log(connectionError);
+        })
+    });
+    app.post("/postBudget", (req, res) => {
+
+        mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+            .then(() => {
+                // Insert
+                console.log("connected to database to insert data");
+                let newData = new budgetModel(req.body);
+                budgetModel.insertMany(newData)
+                    .then((data) => {
+                        res.send("Data Inserted into database Successfully")
+                        mongoose.connection.close();
+                    })
+                    .catch((connectionError) => {
+                        console.log("1", connectionError)
+                        res.send(connectionError.message)
+                    })
+            })
+            .catch((connectionError) => {
+                console.log("2", connectionError)
+                res.send(connectionError);
+            })
+    })
+
+app.get('/hello',(req,res)=>{
+    res.send('Hello world !');
 });
-app.listen(port, () => {
-    console.log('Example app listening at http://localhost:${port}');
+
+app.listen(port, () =>{
+    console.log(`Example app listening at http://localhost:${port}`);
 });
